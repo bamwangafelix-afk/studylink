2d191996f3974ddd00231c01efa64ae6fe72e7f9
bamwangafelix-afk <2.93250958e+08+bamwangafelix-afk@users.noreply.github.com>
2026-09-01 22:42:37 +0000

    Forcer la mise a jour du cache PWA

--- sw-v48.js
+++ sw-v48.js
@@ -1,5 +1,5 @@
-const CACHE='studylink-shell-v58';
-const SHELL=['./','./index.html','./css/styles.css?v=studylink-pwa-58','./js/app-v20.js?v=studylink-pwa-58','./manifest.webmanifest?v=studylink-pwa-58','./icons/studylink-192.png?v=studylink-pwa-58','./icons/studylink-512.png?v=studylink-pwa-58','./icons/studylink-login-full-logo.png?v=studylink-pwa-58'];
+const CACHE='studylink-shell-v59';
+const SHELL=['./','./index.html','./css/styles.css?v=studylink-pwa-59','./js/app-v20.js?v=studylink-pwa-59','./manifest.webmanifest?v=studylink-pwa-59','./icons/studylink-192.png?v=studylink-pwa-59','./icons/studylink-512.png?v=studylink-pwa-59','./icons/studylink-login-full-logo.png?v=studylink-pwa-59'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
 });
