35cb4c4b3e678a08d481eef6da011592e20f7ae5
bamwangafelix-afk <2.93250958e+08+bamwangafelix-afk@users.noreply.github.com>
2026-09-02 15:13:33 +0000

    Creer les messages avant upload arriere-plan

--- sw-v48.js
+++ sw-v48.js
@@ -1,5 +1,5 @@
-const CACHE='studylink-shell-v62';
-const SHELL=['./','./index.html','./css/styles.css?v=studylink-pwa-62','./js/app-v20.js?v=studylink-pwa-62','./manifest.webmanifest?v=studylink-pwa-62','./icons/studylink-192.png?v=studylink-pwa-62','./icons/studylink-512.png?v=studylink-pwa-62','./icons/studylink-login-full-logo.png?v=studylink-pwa-62'];
+const CACHE='studylink-shell-v64';
+const SHELL=['./','./index.html','./css/styles.css?v=studylink-pwa-64','./js/app-v20.js?v=studylink-pwa-64','./manifest.webmanifest?v=studylink-pwa-64','./icons/studylink-192.png?v=studylink-pwa-64','./icons/studylink-512.png?v=studylink-pwa-64','./icons/studylink-login-full-logo.png?v=studylink-pwa-64'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
 });
