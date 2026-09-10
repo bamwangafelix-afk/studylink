9620bcf75e94cae895ac63285bb71b3c5c0dca86
bamwangafelix-afk <2.93250958e+08+bamwangafelix-afk@users.noreply.github.com>
2026-09-10 16:25:59 +0000

    Conserver le déplacement vertical pendant l animation

--- sw-v48.js
+++ sw-v48.js
@@ -1,5 +1,5 @@
-const CACHE='studylink-shell-v77';
-const SHELL=['./','./index.html','./css/styles.css?v=studylink-pwa-77','./js/app-v20.js?v=studylink-pwa-77','./manifest.webmanifest?v=studylink-pwa-77','./icons/studylink-192.png?v=studylink-pwa-77','./icons/studylink-512.png?v=studylink-pwa-77','./icons/studylink-login-full-logo.png?v=studylink-pwa-77'];
+const CACHE='studylink-shell-v78';
+const SHELL=['./','./index.html','./css/styles.css?v=studylink-pwa-78','./js/app-v20.js?v=studylink-pwa-78','./manifest.webmanifest?v=studylink-pwa-78','./icons/studylink-192.png?v=studylink-pwa-78','./icons/studylink-512.png?v=studylink-pwa-78','./icons/studylink-login-full-logo.png?v=studylink-pwa-78'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
 });
