37c0b6444eebe132010644890de0b75d25a7c996
bienaimebangama-star <bienaimebangama@gmail.com>
2026-09-25 17:59:21 +0000

    Refresh cache for orange group actions

--- sw-v48.js
+++ sw-v48.js
@@ -1,5 +1,5 @@
-const CACHE='studylink-shell-v95';
-const SHELL=['./','./index.html','./css/styles.css?v=studylink-pwa-95','./js/app-v20.js?v=studylink-pwa-95','./manifest.webmanifest?v=studylink-pwa-95','./icons/studylink-192.png?v=studylink-pwa-95','./icons/studylink-512.png?v=studylink-pwa-95','./icons/studylink-login-full-logo.png?v=studylink-pwa-95','./icons/person-available.svg?v=studylink-pwa-95'];
+const CACHE='studylink-shell-v96';
+const SHELL=['./','./index.html','./css/styles.css?v=studylink-pwa-96','./js/app-v20.js?v=studylink-pwa-96','./manifest.webmanifest?v=studylink-pwa-96','./icons/studylink-192.png?v=studylink-pwa-96','./icons/studylink-512.png?v=studylink-pwa-96','./icons/studylink-login-full-logo.png?v=studylink-pwa-96','./icons/person-available.svg?v=studylink-pwa-96'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
 });
