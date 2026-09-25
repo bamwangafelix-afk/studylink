0795b7f60158560594e41ab2637849c49abea595
bienaimebangama-star <bienaimebangama@gmail.com>
2026-09-25 13:42:22 +0000

    Refresh service-worker cache for supplied StudyLink release

--- sw-v48.js
+++ sw-v48.js
@@ -1,5 +1,5 @@
-const CACHE='studylink-shell-v84-compat-2';
-const SHELL=['./','./index.html','./css/styles.css?v=studylink-pwa-84','./js/app-v20.js?v=studylink-pwa-84','./manifest.webmanifest?v=studylink-pwa-84','./icons/studylink-192.png?v=studylink-pwa-84','./icons/studylink-512.png?v=studylink-pwa-84','./icons/studylink-login-full-logo.png?v=studylink-pwa-84','./icons/person-available.svg?v=studylink-pwa-84'];
+const CACHE='studylink-shell-v90';
+const SHELL=['./','./index.html','./css/styles.css?v=studylink-pwa-90','./js/app-v20.js?v=studylink-pwa-90','./manifest.webmanifest?v=studylink-pwa-87','./icons/studylink-192.png?v=studylink-pwa-87','./icons/studylink-512.png?v=studylink-pwa-87','./icons/studylink-login-full-logo.png?v=studylink-pwa-87','./icons/person-available.svg?v=studylink-pwa-87'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
 });
